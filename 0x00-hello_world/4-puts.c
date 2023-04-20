f2ef793b328f5c9384c93ddc91dce49f4bead74e
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-20 07:06:45 -0700

    4-puts.c Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+ 1 #include <stdio.h>
+ 2
+ 3 /**
+ 4 * main - Programming is like building a multlingual puzzle
+ 5 * Return: This is 0
+ 6 */
+ 7
+ 8 int main()
+ 9 {
+ 10	 puts("Programming is like building a multilingual puzzle");
+ 11	 return (0);
+ 12 }
