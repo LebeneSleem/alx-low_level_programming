a74eb0ea566b74aea4a66aa3355abff5867a749b
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-20 07:56:08 -0700

    final task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  *
  * Return: This is 0
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
