0618a8f8c0108369f75bb038876e12be3f1b3782
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-20 09:13:58 -0700

    task 5 final

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/**
- * main - with proper grammar, but the outcome is a piece of art
- * return - This is 0
-*/
 
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
