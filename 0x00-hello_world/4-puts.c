506631c2bf08e4417e022d4d0a7b0b5cc9b9d87b
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-20 07:21:29 -0700

    Task 4 corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
- 1 #include <stdio.h>
- 2
- 3 /**
- 4 * main - Programming is like building a multlingual puzzle
- 5 * Return: This is 0
- 6 */
- 7
- 8 int main()
- 9 {
- 10	 puts("Programming is like building a multilingual puzzle");
- 11	 return (0);
- 12 }
+#include <stdio.h>
+/**
+* main - Programming is like building a multlingual puzzle
+* Return: This is 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
