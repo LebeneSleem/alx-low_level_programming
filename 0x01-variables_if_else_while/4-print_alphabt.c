3e43eb5536e716fac1c54581c8a884b1aeb54188
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-21 16:00:18 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - program tonskipnalphabets using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
