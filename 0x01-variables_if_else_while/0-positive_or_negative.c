7f9d15f6ce77265d2cd66c2613eab4ecd77671e8
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-21 10:07:48 -0700

    Task 0 corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("&d is positive\n");
+		printf("&lu is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("&d is zero\n");
+		printf("&lu is zero\n");
 	}
 	else
 	{
-		printf("&d is negative\n");
+		printf("&lu is negative\n");
 	}
 	return (0);
 }
