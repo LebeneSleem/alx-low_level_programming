39eb46f12b0fd5849b4d14aba9542f0e09ae7ae9
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-21 16:55:50 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program to print all single digit numbers of base ten
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
