1a5c451fb61d5ae78c2feec29bee96ffc3e0bb9f
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-04-21 18:47:45 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 48; l < 58; l++)
+	{
+		putchar(l);
+	}
+	if (l != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
