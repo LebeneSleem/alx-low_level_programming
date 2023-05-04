89bf181837a61b2336d2d9ad2bcc9a905d8a9e49
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-04 16:54:52 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,46 +8,46 @@
  * @size_r: buffer size:
  * Return: the pointer to dest.
  */
-#include <string.h>
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
-	int i, j, digit, sum;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	if (len1 + len2 >= size_r - 1)
-	{
-		return (0);
-	}
-	r[len1 + len2] = '\0';
-	i = len1 - 1;
-	j = len2 - 1;
-	while (i >= 0 || j >= 0 || carry)
-	{
-		sum = carry;
-		if (i >= 0)
-		{
-			sum += n1[i] - '0';
-			i--;
-		}
-		if (j >= 0)
-		{
-			sum += n2[j] - '0';
-			j--;
-		}
-		digit = sum % 10;
-		carry = sum / 10;
-		len1--;
-		r[len1 + len2 + carry] = digit + '0';
-	}
-	if (carry == 1)
-	{
-		r[0] = carry + '0';
-		return (&r[0]);
-	}
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
+		bg = c2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		return (&r[len1 + len2 - strlen(r)]);
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+			*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
