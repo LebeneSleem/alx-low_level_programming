14da75569906ff4dc72cc633f352d647909be3da
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-04 06:21:45 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,10 +5,30 @@
  * @str: the string to be capitalized
  * Return: Pointer to the capitalized string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int sle = 0;
 
 	while (str[sle])
 	{
-
+		while (!(str[sle] >= 'a' && str[sle] <= 'z'))
+			sle++;
+		if (str[sle - 1] == ' ' ||
+		str[sle - 1] == '\t' ||
+		str[sle - 1] == '\n' ||
+		str[sle - 1] == ',' ||
+		str[sle - 1] == ';' ||
+		str[sle - 1] == '.' ||
+		str[sle - 1] == '!' ||
+		str[sle - 1] == '?' ||
+		str[sle - 1] == '"' ||
+		str[sle - 1] == '(' ||
+		str[sle - 1] == ')' ||
+		str[sle - 1] == '{' ||
+		str[sle - 1] == '}' ||
+		sle == 0)
+		str[sle] -= 32;
+		sle++;
+	}
+	return (str);
+}
