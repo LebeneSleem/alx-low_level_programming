65fe53d360a774069217e4556c4900e6f064cbdf
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-09 11:32:51 -0700

    updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,14 +9,16 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	printf("0, 1, ");
+
+	while (i < 96)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 97)
+		if (i < 95)
 		{
 			printf(", ");
 		}
