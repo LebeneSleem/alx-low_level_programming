bb1b0c36f40b7fba371f947ddb9ec686b7e7f11d
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-12 11:28:53 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed
+ * @argc: number of arguments
+ * @argv: aray
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
