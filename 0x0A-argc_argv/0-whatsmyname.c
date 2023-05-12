34386715e2ae0d260c815284988650184b356bea
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-12 10:47:50 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints name of programme
+ * @argv: arguement
+ * @argc: string
+ * Return: 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
