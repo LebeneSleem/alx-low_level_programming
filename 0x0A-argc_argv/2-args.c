a856e7b349943cceba387660db7c5bb3f1abed22
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-12 14:31:23 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints arguments
- * @argv - array of arguments
- * @argc : number of arguments
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
  * Return: 0 (Success)
  */
 int main(int argc, char *argv[])
