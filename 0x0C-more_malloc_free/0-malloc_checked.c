c548cb690dd07a98e7b0b008a1da6a3f67dbd3b2
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-18 08:26:24 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: character to be allocated memory for
+ * Return: Pointer to memory allocated
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
