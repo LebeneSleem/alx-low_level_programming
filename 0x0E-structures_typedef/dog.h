ade0421049f40087be0a906d98b73858560c8b4a
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-22 08:17:50 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - struct
+ * @name: name of the dof
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+
+	char *owner;
+
+};
+
+#endif
