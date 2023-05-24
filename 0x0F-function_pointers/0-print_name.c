f1e478194aef89af8dd47cfa8dcad722729d2ade
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-24 06:47:30 -0700

    TAsk 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: string
+ * @f: pointer to function
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)/*check if name and pointer is NULL*/
+		return;
+	f(name);
+}
