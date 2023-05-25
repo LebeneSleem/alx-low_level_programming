cfb7920c0bbdb91c1d539a7cc56b103ea81dda8e
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-25 11:12:08 -0700

    Task 2

--- 0x10-variadic_functions/task_two1.c
+++ 0x10-variadic_functions/task_two1.c
@@ -8,26 +8,31 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list string;
-	const char *ptr;
 	unsigned int list;
-
+	const char *str;
+	
 	va_start(string, n);
-
+	
 	for (list = 0; list < n; list++)
 	{
-		ptr = va_arg(string, const char *);
-		if (ptr == NULL)
+		str = va_arg(string, const char *);
+		
+		if (str == NULL)
 		{
 			printf("(nil)");
 		}
+        else
+        {
+            printf("%s", str);
+        }
 
-		else
-		{
-			printf("%s", ptr);
-		}
-		if (list != (n - 1) && separator != NULL)
-			printf("%s", separator);
-	}
-	printf("\n");
-	va_end(string);
+        if (separator != NULL && i < n - 1)
+        {
+            printf("%s", separator);
+        }
+    }
+
+    va_end(args);
+
+    printf("\n");
 }
