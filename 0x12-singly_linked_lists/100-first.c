03e1c127cecc40f5b5f4d6769de3a51c3f9113eb
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-06-01 13:46:35 -0700

    Advanced Task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_first - function that prints sentences before the main
+ * Return: 0
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
