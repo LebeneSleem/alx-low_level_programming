1d93e576bbac03878b8eb59f92c6214ca251ee12
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-06-06 11:43:30 -0700

    Advanced TAsk 1 part

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function that reverses a listint_t linked list
+ * @head: head of the list
+ * Return: pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+
