4263e5e141139522f51405b0b52ecba6c75e95a9
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-06-12 16:30:22 -0700

    Task 1 - function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	F_D = open(filename, O_CREAT | O_RDWR | O_TRUNC, 06000);
+	F_D = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	w = write(F_D, text_content, length);
 
