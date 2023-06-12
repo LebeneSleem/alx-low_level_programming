c8fb70ea8abe6739bc563b16152d103421329618
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-06-12 16:17:51 -0700

    Task 1 - function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer the file to be created
+ * @text_content: string to be written to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int F_D, w, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	F_D = open(filename, O_CREAT | O_RDWR | O_TRUNC, 06000);
+
+	w = write(F_D, text_content, length);
+
+	if (F_D == -1 || w == -1)
+		return (-1);
+
+	close(F_D);
+
+	return (1);
+}
